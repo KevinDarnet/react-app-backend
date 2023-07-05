d476d7433358d438ffd08cc88e0bf8042c5d28e8
KevinDarnet <kevindarnet.work@gmail.com>
2023-07-05 15:55:12 -0300

    Configuraciones de app necesarias

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-import createError from "http-errors";
 import express from "express";
 import path from "path";
 import logger from "morgan";
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
+import indexRouter from "./routes/index.js";
+import { __dirname } from "./utils.js";
 
 let app = express();
 
@@ -18,23 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//Endpoints... colocamos en indexRouter /api
+app.use("/api", indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+export default app;
